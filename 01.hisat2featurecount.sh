9a49ce48f32f97dfc8342a55f2d35f1c834d194a
Lulu Shi <crazzy_rabbit@163.com>
2023-11-22 15:14:13 +0800

    Update 01.hisat2featurecount.sh

--- 01.hisat2featurecount.sh
+++ 01.hisat2featurecount.sh
@@ -5,11 +5,11 @@
 # ./hismap
 ###############################################################################################################################
 # build index for hisat2
-###############################################################################################################################
+
 hisat2-build -p 4 GCF_910594005.1_mCerEla1.1_genomic.fna  GCF_910594005.1_mCerEla1.1_genomic > hisat2.log
+
 ###############################################################################################################################
 # filter low quality reads and cut adapter use fastp
-###############################################################################################################################
 fastp="/home/sll/miniconda3/bin/fastp"
 
 ls ML*/ML* | cut -d/ -f1 | uniq | while read id; do
@@ -18,9 +18,9 @@ ls ML*/ML* | cut -d/ -f1 | uniq | while read id; do
     -g -q 15 -n 5 -l 150 -u 50 \
     -o ${sample}_1.filter.fq.gz -O ${sample}_2.filter.fq.gz -h ${sample}.fastp.html
 done
+
 ###############################################################################################################################
 # align use hisat2
-###############################################################################################################################
 hisat2="/home/sll/miniconda3/bin/hisat2"
 genomefa="/home/ysq/20221108-deer-depth/20231007-deer-sift-data/reddeer-ref-mCerEla1.1/GCF_910594005.1_mCerEla1.1_genomic"
 MAX_PROCESSES=4
@@ -40,9 +40,9 @@ ls ML*/ML* | cut -d/ -f1 | uniq | while read id; do
         RUNNING_PROCESSES=0
     fi
 done
+
 ###############################################################################################################################
 # sort and reformat use samtools
-###############################################################################################################################
 samtools="/home/sll/miniconda3/bin/samtools"
 MAX_PROCESSES=4
 RUNNING_PROCESSES=0
@@ -65,9 +65,9 @@ ls ML*/ML* | cut -d/ -f1 | uniq | while read id; do
         RUNNING_PROCESSES=0
     fi
 done
+
 ###############################################################################################################################
 # featurecount count the reads number
-###############################################################################################################################
 featureCounts="/home/sll/miniconda3/bin/featureCounts"
 multiqc="/home/sll/miniconda3/bin/multiqc"
 genomegtf="/home/ysq/20221108-deer-depth/20231007-deer-sift-data/reddeer-ref-mCerEla1.1/GCF_910594005.1_mCerEla1.1_genomic.gtf"
@@ -77,4 +77,3 @@ $featureCounts -p -t exon -g gene_id -M -T 8 -a $genomegtf -o all.featurecounts.
 $multiqc all.featurecounts.txt.summary -o  all.counts.summary
 # get counts to R
 awk -F '\t' '{print $1,$6,$7,$8,$9,$10,$11,$12}' OFS='\t' all.featurecounts.txt > all_fcount.matrix.txt
-###############################################################################################################################
