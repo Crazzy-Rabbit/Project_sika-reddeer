7724311010ededd2c1d47933f12a9905c99ca288
Lulu Shi <crazzy_rabbit@163.com>
2023-11-24 10:46:49 +0800

    Update 01.hisat2featurecount.sh

--- 01.hisat2featurecount.sh
+++ 01.hisat2featurecount.sh
@@ -57,6 +57,7 @@ ls ML*/ML* | cut -d/ -f1 | uniq | while read id; do
     if [ -f ${sam_dir}/${sample}.hismap.sam ]; then
         $samtools sort -@ 8 -O bam -o ${bam_dir}/${sample}_sort.bam ${sam_dir}/${sample}.hismap.sam &
         pid=$!
+        RUNNING_PROCESSES=$((RUNNING_PROCESSES+1))
         if ! kill -0 $pid 2>/dev/null; then
             $samtools index ${bam_dir}/${sample}_sort.bam ${bam_dir}/${sample}_sort.bam.index &
         else
@@ -69,14 +70,74 @@ ls ML*/ML* | cut -d/ -f1 | uniq | while read id; do
     fi
 done    
 
+#####################################################
+
+##################################
+### python methon for muti process
+### hisat2 align 
+### samtools sort and index
+### max process = 4
+##################################
+#! usr/bin/python
+import os
+import subprocess
+# muti process
+from concurrent.futures import ThreadPoolExecutor
+
+# set dir that save sam and bam file
+sam_dir = "/home/sll/5t_wgs_20230814_bam/20231118-deer-rna-seq/hismap/sam"
+bam_dir = "/home/sll/5t_wgs_20230814_bam/20231118-deer-rna-seq/hismap"
+hisat2 = "/home/sll/miniconda3/bin/hisat2"
+samtools = "/home/sll/miniconda3/bin/samtools"
+featureCounts = "/home/sll/miniconda3/bin/featureCounts"
+multiqc = "/home/sll/miniconda3/bin/multiqc"
+genomefa = "/home/ysq/20221108-deer-depth/20231007-deer-sift-data/reddeer-ref-mCerEla1.1/GCF_910594005.1_mCerEla1.1_genomic"
+genomegtf = "/home/ysq/20221108-deer-depth/20231007-deer-sift-data/reddeer-ref-mCerEla1.1/GCF_910594005.1_mCerEla1.1_genomic.gtf"
+
+def process_sample(sample):
+    fq_dir = f"/home/sll/5t_wgs_20230814_bam/20231118-deer-rna-seq/{sample}"
+    fq1_file = os.path.join(fq_dir, f"{sample}_1.filter.fq.gz")
+    fq2_file = os.path.join(fq_dir, f"{sample}_2.filter.fq.gz")
+    sam_file = os.path.join(sam_dir, f"{sample}.hismap.sam")
+    bam_file = os.path.join(bam_dir, f"{sample}_sort.bam")
+    index_file = os.path.join(bam_dir, f"{sample}_sort.bam.index")
+
+    # if there are fq1_file and fq2_file
+    if os.path.isfile(fq1_file) and os.path.isfile(fq2_file):
+        # align use hisat2
+        subprocess.run([hisat2, "-p", "8", "-x", genomefa, "-1", fqt1_file, "-2", fqt2_file, "-S", sam_file])
+        # sort sam file
+        subprocess.run([samtools, "sort", "-@", "8", "-O", "bam", "-o", bam_file, sam_file])
+        # index bam file
+        subprocess.run([samtools, "index", bam_file, index_file]) 
+    else:
+        print("Please provide the filter.fq.gz file")
+
+# samples = [id for id in os.listdir(sam_dir) if id.startswith("ML")]
+# samples = subprocess.check_output("ls ML*/ML* | cut -d/ -f1 | uniq", shell=True).decode().splitlines()
+samples = [id.split(".")[0] for id in os.listdir(sam_dir) if id.startswith("ML")]
+
+os.system("mkdir -p ./hismap/sam/")
+
+# set max processes and run
+MAX_PROCESSES = 4
+with ThreadPoolExecutor(max_workers=MAX_PROCESSES) as executor:
+    executor.map(process_sample, samples) 
+
+# run featurecount and multiQC after all file index
+os.system("mkdir /home/sll/5t_wgs_20230814_bam/20231118-deer-rna-seq/hismap/counts")
+os.system(f"$featureCounts -p -t exon -g gene_id -M -T 8 -a $genomegtf -o ./hismap/counts/all.featurecounts.txt *_sort.bam")
+os.system(f"$multiqc ./hismap/counts/all.featurecounts.txt.summary -o  ./hismap/counts/all.counts.summary")
 ###############################################################################################################################
+cd /home/sll/5t_wgs_20230814_bam/20231118-deer-rna-seq/hismap
 # featurecount count the reads number
-featureCounts="~/miniconda3/bin/featureCounts"
-multiqc="~/miniconda3/bin/multiqc"
+featureCounts="/home/sll/miniconda3/bin/featureCounts"
+multiqc="/home/sll/miniconda3/bin/multiqc"
 genomegtf="/home/ysq/20221108-deer-depth/20231007-deer-sift-data/reddeer-ref-mCerEla1.1/GCF_910594005.1_mCerEla1.1_genomic.gtf"
 
-$featureCounts -p -t exon -g gene_id -M -T 8 -a $genomegtf -o all.featurecounts.txt ./hismap.sam/*_sort.bam
+$featureCounts -p -t exon -g gene_id -M -T 8 -a $genomegtf -o ./counts/all.featurecounts.txt *_sort.bam
 # multiqc 
+mkdir counts && cd counts
 $multiqc all.featurecounts.txt.summary -o  all.counts.summary
 # get counts to R
-awk -F '\t' '{print $1,$6,$7,$8,$9,$10,$11,$12}' OFS='\t' all.featurecounts.txt > all_fcount.matrix.txt
+awk -F '\t' '{printf $1; for(i=6;i<=NF;i++) printf FS $i; printf "\n"}' OFS='\t' all.featurecounts.txt > all_fcount.matrix.txt
